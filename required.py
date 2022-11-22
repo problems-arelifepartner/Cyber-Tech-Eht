1f067163fccf38ec5756efe7b4ca97bb6e065b9f
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-11-22 08:27:48 +0530

    Create required.py

---
+++ required.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+print("INSTALLING REQUIRED PACKAGES")
+time.sleep(3)
+print ("THIS IS A EDUCATIONAL TOOL CREATED BY MR.DEVIL AND REDSPY")
+time.sleep(3)
+print ("WE ARE NOT RESPONSIBLE FOR THE ILLEGAL USE OF TOOL BY THE USERS")
+time.sleep(3)
+os.system ("pip2 install mechanize")
+os.system ("pkg install python && pkg install python2 && pkg install python3 && pkg install ruby && pkg install php && pkg install nodejs")
+os.system ("pkg install wget && pkg install curl && pkg install perl && pkg install toilet && pkg install neofetch")
+print ("************************************")
+print ("ALL PACKAGES ARE INSTALLED...TYPE 'bash C3H.sh' to Run the tool")
